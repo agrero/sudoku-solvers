d754fccf3b4e13fcfea22511898dcf82bbfbb3d9
agrero <thegrero@gmail.com>
2024-07-03 15:43:15 -0700

    moved for distinction between package and test

--- tests_package/framework/ConfigFrame.py
+++
@@ -1,8 +0,0 @@
-from sudoku.classes.config.SudokuConfig import SudokuConfig
-import os
-
-class ConfigFrame(SudokuConfig):
-
-    def setUp(self):
-        self.config_path = '/home/alex/coding-projects/sudoku/test.ini' # to be changed
-        self.read_config(verbose=True)
