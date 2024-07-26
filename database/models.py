6fb6ea3a3bbb0b592ca936dbeec32da087bb3d01
agrero <thegrero@gmail.com>
2024-07-26 13:47:23 -0700

    converted tutorial models to useable ones

--- database/models.py
+++ database/models.py
@@ -1,26 +1,24 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
-from database import Base
+from .database import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    prev_puzzle = Column(String, ForeignKey("puzzle.puzzle"))
 
-    items = relationship("Item", back_populates="owner")
+    puzzle = relationship("Puzzle", back_populates="owner")
 
-
-class Item(Base):
-    __tablename__ = "items"
+class Puzzle(Base):
+    __tablename__ = "puzzle"
 
     id = Column(Integer, primary_key=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner_id = Column(Integer, ForeignKey("users.id"), unique=True)
+    puzzle = Column(String, index=True)
 
-    owner = relationship("User", back_populates="items")
+    owner = relationship("User", back_populates="puzzle")
